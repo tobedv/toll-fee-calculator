86b6420f7c242940b2ccb2928243714941c43e07
Tobias Edvardsson <tobias.edvardsson@icomera.com>
2019-01-21 12:30:33 +0100

    Calculator refactor work

--- src/calculator.py
+++ src/calculator.py
@@ -1,122 +1,102 @@
-"""
-Toll fee calculator
-"""
-
-import enum
-
-from .vehicle import Car, Motorbike
+import typing
+import datetime
 
 
-class TollFreeVehicles(enum.Enum):
-    Motorbike = 1
-    Tractor = 2
-    Emergency = 3
-    Diplomat = 4
-    Foreign = 5
-    Military = 6
+class TollCalculator:
+    def __init__(self, config: dict):
+        self.config = config
+        self.max_daily_tax = self.config["max_daily_tax"]
+        self.taxes = []
+        self._initiate_tax_mappings()
+
+    def _initiate_tax_mappings(self):
+        """
+        Parses string times into datetime objects for easier comparison
+        """
+        for period in self.config["tax"]:
+            self.taxes.append(
+                {
+                    "start_time": datetime.time.fromisoformat(period["start_time"]),
+                    "end_time": datetime.time.fromisoformat(period["end_time"]),
+                    "price": period["price"],
+                }
+            )
+
+    def _get_toll_for_time(self, current_time: datetime.datetime) -> int:
+        for period in self.taxes:
+            if period["start_time"] <= current_time.time() <= period["end_time"]:
+                return period["price"]
+
+    def _calculate_max_toll_for_interval(
+        self, passes: typing.List[datetime.datetime]
+    ) -> int:
+        return max([self._get_toll_for_time(p) for p in passes])
+
+    def _build_date_intervals(
+        self, passes: typing.List[datetime.datetime]
+    ) -> typing.List[list]:
+        interval_start = passes[0]
+        intervals = [[interval_start]]
+        current_interval_index = 0
+
+        for next_pass in passes:
+            diff = next_pass - interval_start
+            if diff <= datetime.timedelta(minutes=60):
+                # Add to current interval as long as in the same 60 minutes
+                intervals[current_interval_index].append(next_pass)
+            else:
+                # If bypassing 60 minutes, create new interval, change interval_start for new chunk
+                current_interval_index += 1
+                intervals.append([next_pass])
+                interval_start = next_pass
+        return intervals
+
+    def _get_daily_total_toll_fee(self, passes: typing.List[datetime.datetime]) -> int:
+        """
+        Calculate the total toll fee for one day
+
+        Args:
+            passes (list): Date and time (:py:class:`datetime.datetime`)
+                of all passes on one day, assumes being sorted by date, lower first.
+
+        Returns:
+            int: The total toll fee for that day.
+        """
+
+        total_fee = 0
+        intervals = self._build_date_intervals(passes)
+        for interval in intervals:
+            total_fee += self._calculate_max_toll_for_interval(interval)
+        return min(total_fee, self.max_daily_tax)
 
 
-def get_toll_fee(vehicle, dates):
-    """
-    Calculate the total toll fee for one day
-
-    Args:
-        vehicle (vehicle.Vehicle): The vehicle.
-        dates (list): Date and time (:py:class:`datetime.datetime`)
-            of all passes on one day.
-
-    Returns:
-        int: The total toll fee for that day.
-    """
-    interval_start = dates[0]
-    total_fee = 0
-
-    for i in range(len(dates)):
-        next_fee = toll_fee_passage(dates[i], vehicle)
-        temp_fee = toll_fee_passage(interval_start, vehicle)
-        diff = dates[i] - interval_start
-        minutes = diff.total_seconds() // 60
-
-        if minutes <= 60:
-            if total_fee:
-                total_fee -= temp_fee
-            if next_fee >= temp_fee:
-                temp_fee = next_fee
-            total_fee += temp_fee
-        else:
-            total_fee += next_fee
-
-    return min(total_fee, 60)
-
-
-def toll_fee_passage(date, vehicle):
-    """
-    Return the fee for the passage at `date`.
-
-    Args:
-        date (datetime.datetime): The passage date.
-        vehicle (vehicle.Vehicle): The vehicle.
-
-    Returns:
-        int: The toll fee for the passage
-    """
-    if _is_toll_free_date(date) or _is_toll_free_vehicle(vehicle):
-        return 0
-
-    hour = date.hour
-    minute = date.minute
-
-    if hour == 6 and minute >= 0 and minute <= 29:
-        return 9
-    elif hour == 6 and minute >= 30 and minute <= 59:
-        return 16
-    elif hour == 7 and minute >= 0 and minute <= 59:
-        return 22
-    elif hour == 8 and minute >= 0 and minute <= 29:
-        return 16
-    elif hour >= 8 and hour <= 14 and minute <= 29:
-        return 9
-    elif hour == 15 and minute >= 0 and minute <= 29:
-        return 16
-    elif hour == 15 and minute >= 0 or hour == 16 and minute <= 59:
-        return 22
-    elif hour == 17 and minute >= 0 and minute <= 59:
-        return 16
-    elif hour == 18 and minute >= 0 and minute <= 29:
-        return 9
-    else:
-        return 0
-
-
-def _is_toll_free_date(date):
-    if date.weekday() in (5, 6):
-        return True
-
-    elif date.year == 2018:
-        if (date.month == 1 and (date.day == 1 or date.day == 5 or date.day == 6)
-                or date.month == 3 and (date.day == 29 or date.day == 30)
-                or date.month == 4 and (date.day == 2 or date.day == 30)
-                or date.month == 5 and (date.day == 1 or date.day == 9 or date.day == 10)
-                or date.month == 6 and (date.day == 5 or date.day == 6 or date.day == 22)
-                or date.month == 7
-                or date.month == 11 and date.day == 2
-                or date.month == 12 and (date.day == 24 or date.day == 25 or date.day == 26 or date.day == 31)
-            ):
-            return True
-        else:
-            return False
-    return False
-
-
-def _is_toll_free_vehicle(vehicle):
-    vehicle_type = vehicle.get_type()
-    if vehicle == None:
-        return False
-
-    return (
-        vehicle_type == TollFreeVehicles.Motorbike.name or
-        vehicle_type == TollFreeVehicles.Tractor.name or
-        vehicle_type == TollFreeVehicles.Emergency.name or
-        vehicle_type == TollFreeVehicles.Diplomat.name or
-        vehicle_type == TollFreeVehicles.Foreign.name or
-        vehicle_type == TollFreeVehicles.Military.name)
+if __name__ == "__main__":
+    cfg = {
+        "max_daily_tax": 60,
+        "tax": [
+            {"start_time": "06:00", "end_time": "06:29", "price": 9},
+            {"start_time": "06:30", "end_time": "06:59", "price": 16},
+            {"start_time": "07:00", "end_time": "07:59", "price": 22},
+            {"start_time": "08:00", "end_time": "08:29", "price": 16},
+            {"start_time": "08:30", "end_time": "14:59", "price": 9},
+            {"start_time": "15:00", "end_time": "15:29", "price": 16},
+            {"start_time": "15:30", "end_time": "16:59", "price": 22},
+            {"start_time": "17:00", "end_time": "17:59", "price": 16},
+            {"start_time": "18:00", "end_time": "18:29", "price": 9},
+            {"start_time": "18:30", "end_time": "05:59", "price": 0},
+        ],
+    }
+    tc = TollCalculator(cfg)
+    test_data = [
+        datetime.datetime.now(),
+        datetime.datetime.now() + datetime.timedelta(minutes=20),
+        datetime.datetime.now() + datetime.timedelta(minutes=40),
+        datetime.datetime.now() + datetime.timedelta(minutes=59),
+        # Second
+        datetime.datetime.now() + datetime.timedelta(minutes=60),
+        datetime.datetime.now() + datetime.timedelta(minutes=65),
+        # Third
+        datetime.datetime.now() + datetime.timedelta(minutes=150),
+    ]
+    r = tc._get_daily_total_toll_fee(test_data)
+    print(r)
---
+++ tools/date_generator.py
@@ -0,0 +1,72 @@
+import datetime
+import json
+
+
+def generate_july_dates(year):
+    dates = []
+    start = datetime.datetime(year=2019, month=7, day=1)
+    for day in range(2, 32):
+        dates.append(start.date())
+        start = start.replace(day=day)
+    return dates
+
+
+def generate_red_and_free_days(red_days):
+    dates = []
+    # The day before a red day should be included as free
+    for day in red_days:
+        current_day = datetime.datetime.strptime(day, "%Y-%m-%d")
+        day_before = current_day - datetime.timedelta(days=1)
+        dates.append(day_before.date())
+        dates.append(current_day.date())
+    return dates
+
+
+if __name__ == "__main__":
+    """
+    Red days are fetched from, https://publicholidays.se/sv/2019-dates/
+    Free days from https://transportstyrelsen.se/sv/vagtrafik/Trangselskatt/Betalning/dagar-da-trangselskatt-inte-tas-ut/
+    """
+    year = 2019
+    red_days = [
+        "2019-01-01",
+        "2019-01-06",
+        "2019-04-19",
+        "2019-04-21",
+        "2019-04-22",
+        "2019-05-01",
+        "2019-05-30",
+        "2019-06-06",
+        "2019-06-09",
+        "2019-06-22",
+        "2019-11-02",
+        "2019-12-25",
+        "2019-12-26",
+    ]
+    free_days = [
+        "2019-01-01",
+        "2019-04-18",
+        "2019-04-19",
+        "2019-05-01",
+        "2019-06-05",
+        "2019-06-06",
+        "2019-06-21",
+        "2019-11-01",
+        "2019-12-24",
+        "2019-12-25",
+        "2019-12-26",
+        "2019-12-31",
+    ]
+    july = generate_july_dates(year)
+    red_and_free = generate_red_and_free_days(red_days)
+    all_dates = (
+        july
+        + red_and_free
+        + [datetime.datetime.strptime(day, "%Y-%m-%d").date() for day in free_days]
+    )
+    # set to filter out duplicates, if clause to remove wrong year (should only be 2018-12-31)
+    # sort, and get string representation for date, could be implemented as custom json decoder
+    filtered_dates = list({date for date in all_dates if date.year == 2019})
+    filtered_dates.sort()
+    filtered_dates = [str(date) for date in filtered_dates]
+    print(json.dumps(list(filtered_dates)))
